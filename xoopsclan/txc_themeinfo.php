5b0a27b35f4068759a58438f9d0ddab4739a1d66
mambax7 <mambax7@gmail.com>
2015-06-06 10:04:21 -0400

    Original files

---
+++ xoopsclan/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "XOOPSclan";
+	$txc_themeinfo["author"] = "eirenicon";
+	$txc_themeinfo["author_url"] = "http://www.eirenicon.org";
+	$txc_themeinfo["detail_url"] = "http://www.eirenicon.org";
+	$txc_themeinfo["download_url"] = "http://www.eirenicon.org";
+?>
